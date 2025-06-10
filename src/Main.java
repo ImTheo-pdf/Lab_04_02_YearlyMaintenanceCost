6ddc36f0a2750f0160f5035d948bf7b3229ad91f
ImTheo-pdf <trejacks223@gmail.com>
2025-06-09 22:49:38 -0400

    Finished Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double springMaintenanceCost = 150.00;
+        double summerMaintenanceCost = 300.00;
+        double fallMaintenanceCost = 125.00;
+        double winterMaintenanceCost = 400.00;
+        double totalMaintenanceCost;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        totalMaintenanceCost = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
+
+        System.out.println("Spring Maintenance Cost: $" + springMaintenanceCost);
+        System.out.println("Summer Maintenance Cost: $" + summerMaintenanceCost);
+        System.out.println("Fall Maintenance Cost: $" + fallMaintenanceCost);
+        System.out.println("Winter Maintenance Cost: $" + winterMaintenanceCost);
+        System.out.println("Total yearly Maintenance Cost: $" + totalMaintenanceCost);
     }
 }
